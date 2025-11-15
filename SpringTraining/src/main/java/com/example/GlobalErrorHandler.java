424b26aef4b01ef73150a413cf26b4fb52e2d7a6
Shubhranshu012 <shubhranshu.satpathy@gmail.com>
2025-11-15 17:50:51 +0530

    Validation

---
+++ SpringTraining/src/main/java/com/example/GlobalErrorHandler.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalErrorHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleException(MethodArgumentNotValidException exception) {
+        
+        Map<String, String> errorMap = new HashMap<>();
+        List<ObjectError> errorsList = exception.getBindingResult().getAllErrors();
+        
+        errorsList.forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errorMap.put(fieldName, message);
+        });
+        
+        return errorMap;
+    }
+}
---
+++ SpringTraining/src/main/java/com/example/request/Order1.java
@@ -0,0 +1,49 @@
+package com.example.request;
+
+import org.hibernate.validator.constraints.Range;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+@Entity
+public class Order1 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	int id;
+	private String item;
+	private float price;
+	private int quantity;
+	
+	public String getItem() {
+		return item;
+	}
+	public void setItem(String item) {
+        this.item = item;
+    }
+	public int getQuantity() {
+        return quantity;
+    }
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
